7a8b9b54e0bedeb3d21e7e6ff38e1f2731f1e841
Jeanluca Valencia <jeanlucavalencia08@gmail.com>
2026-04-12 00:32:11 -0700

    Set up Spring Boot project

---
+++ src/main/java/com/jeanlucavalencia/ufcmanager/UfcmanagerApplication.java
@@ -0,0 +1,14 @@
+package com.jeanlucavalencia.ufcmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfcmanagerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UfcmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeanlucavalencia/ufcmanager/UfcmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanlucavalencia.ufcmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfcmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
