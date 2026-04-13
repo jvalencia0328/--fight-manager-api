e654307e346ff9e8b406a785fea27a49bd236028
Jeanluca Valencia <jeanlucavalencia08@gmail.com>
2026-04-13 01:22:41 -0700

    Fix file type for FighterRepository and finish basic setup

--- src/main/java/com/jeanlucavalencia/ufcmanager/repository/FighterRepository.java
+++ src/main/java/com/jeanlucavalencia/ufcmanager/repository/FighterRepository.java
@@ -1,1 +1,6 @@
 package com.jeanlucavalencia.ufcmanager.repository;
+import com.jeanlucavalencia.ufcmanager.entity.Fighter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FighterRepository extends JpaRepository<Fighter, Long> {
+}
